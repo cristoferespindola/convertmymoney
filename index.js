58f9716e96067c483f63c0bad2c45c6984cd5c12
Cristofer <cristoferespindola@outlook.com>
2019-12-20 19:19:40 -0300

    Atualizando

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const convert = require('./lib/convert')
+const apiBc = require('./lib/apiBc')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.render('home')
+app.get('/', async(req, res) => {
+  const quotation = await apiBc.getQuotation('04-12-2019')
+
+  res.render('home', {
+    quotation
+  })
 })
 
 app.get('/quotation', (req, res) => {
